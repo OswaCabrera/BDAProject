f622ca713eac369f3dc9abd1ac5734f550ddc683
OswaCabrera <oswaldo_cabreracch@comunidad.unam.mx>
2023-01-22 13:32:54 -0600

    Asignamos tablas a usuarios

--- Scripts/s-14-crear-usuarios.sql
+++ Scripts/s-14-crear-usuarios.sql
@@ -41,3 +41,36 @@ create user media_admin  identified by gaca123 quota unlimited on media
   default tablespace media;
 grant create session, create table, create procedure, create view,
   create synonym, create sequence to media_admin;
+
+grant select insert, update, delete on sys.empresa to client_admin;
+grant select, insert, update, delete on sys.persona to client_admin;
+grant select, insert, update, delete on sys.tarjeta to client_admin;
+grant select, insert, update, delete on sys.pago to client_admin;
+grant select, insert, update, delete on sys.cliente to client_admin;
+
+grant select, insert, update, delete on sys.proveedor to vendor_admin;
+grant select, insert, update, delete on sys.banco to vendor_admin;
+grant select, insert, update, delete on sys.deposito to vendor_admin;
+grant select, insert, update, delete on sys.nivel_estudio to vendor_admin;
+grant select, insert, update, delete on sys.email to vendor_admin;
+grant select, insert, update, delete on sys.imagen to vendor_admin;
+grant select, insert, update, delete on sys.evidencia_servicio to vendor_admin;
+grant select, insert, update, delete on sys.estatus_proveedor to vendor_admin;
+grant select, insert, update, delete on sys.proveedor_tipo_servicio to vendor_admin;
+grant select, insert, update, delete on sys.comprobante_experiencia to vendor_admin;
+grant select, insert, update, delete on sys.entidad_federativa to vendor_admin;
+
+grant select, insert, update, delete on sys.servicio to service_admin;
+grant select, insert, update, delete on sys.tipo_servicio to service_admin;
+grant select, insert, update, delete on sys.estatus_servicio to service_admin;
+grant select, insert, update, delete on sys.historico_estatus_servicio to service_admin;
+grant select, insert, update, delete on sys.calificacion to service_admin;
+
+grant select, insert, update, delete on sys.cuenta_bancaria to bank_admin;
+grant select, insert, update, delete on sys.banco to bank_admin;
+grant select, insert, update, delete on sys.tarjeta to bank_admin;
+
+grant select, insert, update, delete on sys.imagen to media_admin;
+grant select, insert, update, delete on sys.cuenta_bancaria to bank_admin;
+
+
